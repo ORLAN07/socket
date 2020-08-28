74f152748fd2b8b6ed7c359b93f5f407ad2d8cd8
OrlandoRNX <orlandorivasrubio@live.com>
2020-08-28 14:45:56 -0500

    changes

--- src/com/rnx/Main.java
+++ src/com/rnx/Main.java
@@ -1,8 +1,34 @@
 package com.rnx;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private static int PORT = 9000;
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        Socket socket = null;
+
+        DataInputStream dataInputStream = null;
+        DataOutputStream dataOutputStream = null;
+
+        while(true){
+
+            socket = serverSocket.accept();
+            System.out.println("listen to client 1");
+
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            System.out.println("in put: " + dataInputStream.readUTF());
+            dataOutputStream.writeUTF("Server Orlando on Port 9000");
+            socket.close();
+        }
     }
 }
